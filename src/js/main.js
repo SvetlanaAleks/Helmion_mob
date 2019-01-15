08ebd0cc22b59da22d741e4bb822383e20789715
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-15 10:56:44 +0200

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
-
+//=require ../../node_modules/slick-carousel/slick/slick.min.js
 //=require partials/app.js
+
+$(document).ready(function () {
+   App.init();
+});
+
+$('.fix__btn').click(function (e) {
+   var $message1 = $('.menu');
+
+   if ($message1.css('display') != 'block') {
+      $message1.show();
+      var firstClick = true;
+      $(document).bind('click.myEvent', function (e) {
+         if (!firstClick && $(e.target).closest('.menu').length == 0) {
+            $message1.hide();
+            $(document).unbind('click.myEvent');
+         }
+         firstClick = false;
+      });
+   }
+
+   e.preventDefault();
+});
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,43 @@
 //Some JS
+const App = (function () {
+   "use strict";
+   return {
+      scrollToTarget: function (scrollSelector, speed) {
+         const links = $(scrollSelector);
+         links.click(function (e) {
+            e.preventDefault();
+            const _this = $(this);
+            const href = _this.attr('href');
+
+            if (href.length <= 1) {
+               return;
+            }
+            const target = $(href);
+            if (!target.length) {
+               return;
+            }
+            const top = target.offset().top;
+            $('html, body').animate({
+               scrollTop: top
+            }, speed);
+         });
+      },
+      sliderReviewsInit: function () {
+         $('.reviews__slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: "linear",
+            prevArrow: '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
+            nextArrow: '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
+            autoplay: false,
+            adaptiveHeight: true
+         });
+      },
+      init: function () {
+         App.scrollToTarget('.js-scroll', 700);
+         App.sliderReviewsInit();
+      },
+   };
+})();
